369b1b1e3c32500bcbf89a2f57250db0c44cec8b
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-09-18 17:38:38 +0100

    Chore: Bypass parallel fetch, build system updates
    Optionally allow bypass of parallel credential fetch
    Rename no_tags -> hide_tags
    Fix cosmetic documentation error
    Migrate builds to pdm backend and pyproject.toml
    Update linting environment
    Implement some very basic initial tests
    
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>
    Change-Id: I46b3ad2fc19b4f7e7d7893db7b39c675556d5a94

---
+++ tests/test_python_one_password.py
@@ -0,0 +1,26 @@
+__author__ = "Matthew Watkins"
+__copyright__ = "Matthew Watkins"
+__license__ = "Apache-2.0"
+
+# External modules
+# import typer
+
+# Testing modules
+from typer.testing import CliRunner
+
+# Bundled modules
+from python_one_password.tags import app as tags
+
+runner = CliRunner()
+
+
+# Test that we throw an error when no arguments are supplied
+def test_error():
+    result = runner.invoke(tags)
+    assert result.exit_code == 2
+
+
+# Test that main command and all sub-commands have working help
+def test_help():
+    result = runner.invoke(tags, ["--help"])
+    assert result.exit_code == 0
